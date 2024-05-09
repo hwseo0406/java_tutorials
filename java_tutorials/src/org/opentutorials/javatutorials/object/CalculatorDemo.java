8c1b17fc2db768324c155ee050a66d091dcde6e6
user <user@LAPTOP-LGOJMRFU>
2024-05-09 18:29:14 +0900

    add object

---
+++ java_tutorials/src/org/opentutorials/javatutorials/object/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.object;
+ 
+public class CalculatorDemo {
+ 
+    public static void main(String[] args) {
+        // 아래의 로직이 1000줄 짜리의 복잡한 로직이라고 가정하자.
+        System.out.println(10 + 20); //+는 1000자짜리 코드라고 하면 중복이다
+        System.out.println(20 + 40); //중복의 제거가 효율적임
+    }
+ 
+}
