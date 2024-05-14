235d148d46d676af2f18869f88ab0acb25fcf778
hwseo0406 <hse05078@gmail.com>
2024-05-14 16:19:54 +0900

    test push

--- java_tutorials/src/org/opentutorials/javatutorials/array/DefineDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/array/DefineDemo.java
@@ -6,7 +6,7 @@ public class DefineDemo {
 		String[] classGroup = { "최진혁", "최유빈", "한이람", "이고잉" }; //배열을 중괄호로 시작하고 끝남
 		int[] a = {1,2,3,4};
 		//배열은 데이터타입별로 지정도 가능
-		System.out.println(classGroup[1] + a[2]);
+		System.out.println(classGroup[1] + a[2]); //a
 
 	}
 
