fbea8ecadf8a28233ac0b80738303bd797f9d93b
hwseo0406 <hse05078@gmail.com>
2024-05-15 13:53:01 +0900

    2024 Buddha's Birthday holiday_cram first_section

--- java_tutorials/src/org/opentutorials/javatutorials/classinstance/CalculatorDemo2.java
+++ java_tutorials/src/org/opentutorials/javatutorials/classinstance/CalculatorDemo2.java
@@ -2,8 +2,7 @@ package org.opentutorials.javatutorials.classinstance;
  
 class Calculator2 {
     static double PI = 3.14;
-    // 클래스 변수인 base가 추가되었다.
-    static int base = 0;
+    static int base = 0; //클래스 변수 base 추가
     int left, right;
  
     public void setOprands(int left, int right) {
@@ -12,12 +11,10 @@ class Calculator2 {
     }
  
     public void sum() {
-        // 더하기에 base의 값을 포함시킨다.
         System.out.println(this.left + this.right + base);
     }
  
     public void avg() {
-        // 평균치에 base의 값을 포함시킨다.
         System.out.println((this.left + this.right + base) / 2);
     }
 }
@@ -28,22 +25,17 @@ public class CalculatorDemo2 {
  
         Calculator2 c1 = new Calculator2();
         c1.setOprands(10, 20);
-        // 30 출력
-        c1.sum();
+        c1.sum(); // 30 출력
  
         Calculator2 c2 = new Calculator2();
         c2.setOprands(20, 40);
-        // 60 출력
-        c2.sum();
- 
-        // 클래스 변수 base의 값을 10으로 지정했다.
-        Calculator2.base = 10;
- 
-        // 40 출력
-        c1.sum();
- 
-        // 70 출력
-        c2.sum();
+        c2.sum(); // 60 출력
+
+        Calculator2.base = 10; // 클래스 변수 base의 값을 10으로 지정했다.
+
+        c1.sum(); // 40 출력
+
+        c2.sum(); // 70 출력
  
     }
  
--- java_tutorials/src/org/opentutorials/javatutorials/scope/ScopeDemo5.java
+++ java_tutorials/src/org/opentutorials/javatutorials/scope/ScopeDemo5.java
@@ -1,16 +1,11 @@
 package org.opentutorials.javatutorials.scope;
- 
-public class ScopeDemo {
- 
-    static void a() {
-        int i = 0; //이건 이 메소드 안에서만 유효함
-    }
- 
+
+public class ScopeDemo5 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            a();
             System.out.println(i);
         }
+        int i = 0;
+        System.out.println(i);
     }
- 
 }
