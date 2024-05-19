0553bd34e329c29144cae4ac63d2703697ee8fb7
hwseo0406 <hse05078@gmail.com>
2024-05-20 00:50:00 +0900

    20240520_00:48_after_futsal_util_API

---
+++ java_tutorials/src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,16 @@
+//package org.opentutorials.javatutorials.library;
+//
+//public class LibraryDemo1 {
+//    public static void main(String[] args) {
+//        System.out.println(1); //System은 class, out은 System의 객체, println은 out의 멤버
+//    }
+//}
+
+package org.opentutorials.javatutorials.library;
+import java.lang.*; //이 코드는 import가 안되어있으면 자동으로 함.
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
+
